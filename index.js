2e92f87e15f644cb444ca03c1affeb2c44679bfe
iMadfxq <imadfxq@gmail.com>
2022-02-11 21:18:11 -0600

    Finished logic to see if answers are correct

---
+++ index.js
@@ -0,0 +1,19 @@
+const correctAnswers = ["B", "A", "B", "B"]
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  
+  let score = 0
+
+  let userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+  
+  userAnswers.forEach((answer, index) => {
+    if(answer === correctAnswers[index]){
+      score += 25
+    }
+  })
+
+  console.log(score)
+})
